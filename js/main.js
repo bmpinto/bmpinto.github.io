bfc09d843138110490bcda17e98236841c079efc
Bruno Pinto <bmorosopinto@gmail.com>
2017-01-16 23:00:13 +0000

    Inicio mediaqueries

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
+function runScroll() {
+  scrollTo(document.body, 0, 600);
+}
+var scrollme;
+scrollme = document.querySelector("#logo");
+scrollme.addEventListener("click",runScroll,false)
+
+function scrollTo(element, to, duration) {
+  if (duration <= 0) return;
+  var difference = to - element.scrollTop;
+  var perTick = difference / duration * 10;
+
+  setTimeout(function() {
+    element.scrollTop = element.scrollTop + perTick;
+    if (element.scrollTop == to) return;
+    scrollTo(element, to, duration - 10);
+  }, 10);
+}
+
 var menuTrigger = document.querySelector('.menu-trigger')
 
 menuTrigger.addEventListener('click', function() {
